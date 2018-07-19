56b5a6c68bd64ad980a539393c35e0f4692abe24
msinoplis <msinoplis@spartaglobal.com>
2018-07-19 14:42:55 +0100

    add provision.sh

---
+++ provision.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+sudo apt-get upgrade -y
+
+sudo apt-get install nginx -y
+
+sudo apt-get install python-software-properties
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
+
+sudo apt-get install nodejs -y
+
+sudo apt-get install npm -y
+
+sudo npm install pm2 -g
